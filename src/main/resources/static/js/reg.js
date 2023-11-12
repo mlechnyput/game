99d52e5a0eeedf2c03b5abc04af68bfa4d79471b
mlechnyput <mlechnyput1@gmail.com>
2023-11-12 14:26:48 +0300

    Finished /register

--- src/main/resources/static/js/reg.js
+++ src/main/resources/static/js/reg.js
@@ -1,28 +1,64 @@
-async function reg() {
+const r_form = document.getElementById('reg_form');
 
-    const urlReg = "http://localhost:8080/register"
+r_form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    console.log('prevent submitting');
+})
 
-    const visitorReg = {
-        // username: $('#reg_username').val(),
-        // password: $('#reg_password').val()
-        username:'Vasya',
-        password:'mgh'
-    }
-
-    const headers = {
-        'Content-Type': 'application/json'
-    }
-
-    try {
-        const response = await fetch(urlReg, {
-            method: 'POST',
-            body: JSON.stringify(visitorReg),
-            headers: headers
-        })
-        const txt=await response.text()
-        console.log('Успех: ', txt)
-    }catch (error){
-        console.error('Ошибка: ',error)
-    }
+function containsWhitespace(str) {
+    return /\s/.test(str);
 }
 
+const str = document.getElementById('reg_str');
+
+const base = "http://localhost:8080/";
+
+async function reg(var_u, var_p) {
+    str.innerText = '';
+    if (var_u.length < 3 || var_p.length < 3) {
+        str.innerText = 'Слишком короткое имя или пароль'
+    } else {
+        if (containsWhitespace(var_u) || containsWhitespace(var_p)) {
+            str.innerText = 'Убери пробелы и попробуй снова';
+        } else {
+
+            const visitorReg = {
+                username: var_u,
+                password: var_p
+            };
+
+            const headers = {
+                'Content-Type': 'application/json'
+            };
+
+            try {
+                const response_reg = await fetch(`${base}register`, {
+                    method: 'POST',
+                    body: JSON.stringify(visitorReg),
+                    headers: headers
+                });
+                const txt_reg = await response_reg.text();
+                console.log('Успех: ', txt_reg);
+                if (txt_reg === 'busy') {
+                    str.innerText = 'Имя занято, попробуй другое';
+                } else {
+                    if (txt_reg === 'saved') {
+                        const response_come = await fetch(`${base}come`, {
+                            method: 'POST',
+                            body: `username=${var_u}&password=${var_p}`,
+                            headers: {
+                                'Content-Type': 'application/x-www-form-urlencoded'
+                            }
+                        });
+                        const status = response_come.status;
+                        if (status == 200) {
+                            window.location.replace(base);
+                        }
+                    }
+                }
+            } catch (error) {
+                console.error('Ошибка: ', error);
+            }
+        }
+    }
+}
