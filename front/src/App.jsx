eb5bea10fc577e2b9139546c96e5ffe60eb398c2
mlechnyput <mlechnyput1@gmail.com>
2023-11-14 19:18:17 +0300

    username and score are shown in front

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,5 +1,4 @@
-import React, {useState} from "react";
-import "./styles.css";
+import React, {} from "react";
 import Screen from "./Screen";
 
 export default function App() {
--- front/src/Screen.jsx
+++ front/src/Screen.jsx
@@ -1,13 +1,42 @@
-import React, {useState} from "react";
-
+import React, {useEffect, useState} from "react";
+import frontProperties from "./front.properties.json"
 import "./styles.css";
 
 export default function Screen() {
 
+    const [player, setPlayer] = useState({
+        username: '...',
+        score: -999
+    })
+
+    useEffect(() => {
+        const ws = new WebSocket(`${frontProperties.ws_base_url}chat`);
+
+        ws.onopen = function () {
+            console.log('connected');
+            const obj = {
+                type: 'HANDSHAKE_REQUEST'
+            };
+            const json_msg = JSON.stringify(obj);
+            ws.send(json_msg);
+        };
+
+        ws.onmessage = function (event) {
+            console.log('msg')
+            console.log(event.data)
+            const gotFromBack = JSON.parse(event.data)
+            switch (gotFromBack.type) {
+                case 'HANDSHAKE_RESPONSE':
+                    setPlayer(gotFromBack.body);
+                    break;
+            }
+        };
+    }, [])
+
     return (
         <div className="body">
-            <div className="player">player</div>
-            <div className="hat">header</div>
+            <div className="player">{player.username}</div>
+            <div className="hat">{player.score}</div>
             <div className="field">field</div>
             <div className="chat">chat</div>
             <div className="chat_control">chat control</div>
