2e5e85ed935fc64d1e7b61f49d6638fe1455917f
mlechnyput <mlechnyput1@gmail.com>
2023-12-06 22:56:44 +0300

    -4000 протестировал и настроил

--- front/src/Field.jsx
+++ front/src/Field.jsx
@@ -736,23 +736,24 @@ function Field() {
                 let compensation_x;
                 let compensation_y;
                 let rotation;
-                if (r.hit_area === 'body') {
-                    compensation_x = 87;
-                    compensation_y = -30;
-                    rotation = 90;
-                } else {
-                    if (r.hit_area === 'legs') {
+
+                if (r.hit_area === 'legs' || r.hit_area === 'body') {
+                    if (r.angle_degree <= -55) {
+                        compensation_x = 160;
+                        compensation_y = 20;
+                    } else if (r.angle_degree <= -20) {
                         compensation_x = 100;
                         compensation_y = -30;
-                        rotation = 90;
-                    } else {
-                        if (r.hit_area === 'apple') {
-                            compensation_x = 100;
-                            compensation_y = -90;
-                            rotation = 90 + r.angle_degree;
-                        }
+                    }
+                    rotation = 90;
+                } else {
+                    if (r.hit_area === 'apple') {
+                        compensation_x = 100;
+                        compensation_y = -90;
+                        rotation = 90 + r.angle_degree;
                     }
                 }
+
                 arrow_stick_ref.current.style.transform = 'rotate(' + rotation + 'deg)';
                 arrow_stick_ref.current.style.left = r.coord_x + compensation_x + 'px';
                 arrow_stick_ref.current.style.top = r.coord_y + compensation_y + 'px';
