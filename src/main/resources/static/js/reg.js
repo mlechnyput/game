35ee19d0dbe38a3c76a5dc951929ad37496f5e1f
mlechnyput <mlechnyput1@gmail.com>
2023-12-30 02:40:37 +0300

    форма регистрации - двуязычная

--- src/main/resources/static/js/reg.js
+++ src/main/resources/static/js/reg.js
@@ -10,19 +10,71 @@ function containsWhitespace(str) {
 }
 
 const str = document.getElementById('reg_str');
+const hat_str = document.getElementById('hat');
+const language_str = document.getElementById('language');
+const nick_str = document.getElementById('nick');
+const password_str = document.getElementById('password');
+const back_str = document.getElementById('back');
+const send_str = document.getElementById('send');
 
 const base = "http://localhost:8080/";
+
+let language = 'RUS';
+
+function changeLanguage() {
+    str.innerText = '';
+    if (language === 'RUS') {
+        hat_str.innerText = 'Sign Up';
+        language_str.innerText = 'RUS';
+        nick_str.innerText = 'Nick Name';
+        password_str.innerText = "Password";
+        back_str.innerText = 'Back to Login';
+        send_str.value = 'Send';
+        language = 'EN';
+    } else {
+        if (language === 'EN') {
+            hat_str.innerText = 'Регистрация';
+            language_str.innerText = 'EN';
+            nick_str.innerText = 'Погонялово';
+            password_str.innerText = 'Пароль';
+            back_str.innerText = 'Назад ко входу';
+            send_str.value = 'Отправить';
+            language = 'RUS';
+        }
+    }
+}
 
 async function reg(var_u, var_p) {
     str.innerText = '';
     if (var_u.length > 16) {
-        str.innerText = 'Имя не должно превышать 16 символов'
+        switch (language) {
+            case "RUS":
+                str.innerText = 'Имя не должно превышать 16 символов';
+                break;
+            case "EN":
+                str.innerText = 'Nick Name must be less 16 symbols';
+                break;
+        }
     } else {
         if (var_u.length < 3 || var_p.length < 3) {
-            str.innerText = 'Слишком короткое имя или пароль'
+            switch (language) {
+                case "RUS":
+                    str.innerText = 'Слишком короткое имя или пароль'
+                    break;
+                case "EN":
+                    str.innerText = 'Too short Nick Name or Password';
+                    break;
+            }
         } else {
             if (containsWhitespace(var_u) || containsWhitespace(var_p)) {
-                str.innerText = 'Убери пробелы и попробуй снова';
+                switch (language) {
+                    case "RUS":
+                        str.innerText = 'Убери пробелы и попробуй снова';
+                        break;
+                    case "EN":
+                        str.innerText = 'Delete whitespaces and try again';
+                        break;
+                }
             } else {
 
                 const visitorReg = {
